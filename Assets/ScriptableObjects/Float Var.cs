3e8f667ddf056321ba289c66a94b7f648983661b
legendofnothing <huyrtb123@gmail.com>
2022-06-11 12:03:00 +0700

    Add Scriptable Object float

---
+++ Assets/ScriptableObjects/Float Var.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Float Var", menuName = "ScriptableObjects/Float Vars", order = 1)]
+
+public class FloatVar : ScriptableObject {
+    public float Value;
+}
