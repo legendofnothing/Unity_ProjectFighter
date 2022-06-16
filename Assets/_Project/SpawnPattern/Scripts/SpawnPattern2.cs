b2672d806518029b71a08d1a0bbc8397d1c0fa65
legendofnothing <huyrtb123@gmail.com>
2022-06-16 12:38:15 +0700

    Nerf Turrets, Fixing Boss Sorting Layer

--- Assets/_Project/SpawnPattern/Scripts/SpawnPattern2.cs
+++ Assets/_Project/SpawnPattern/Scripts/SpawnPattern2.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SpawnPattern2 : MonoBehaviour {
-    private GameObject[] spawners;
+    public GameObject[] spawners;
 
     #region Unity Methods
     void Start() {
@@ -20,7 +20,32 @@ public class SpawnPattern2 : MonoBehaviour {
     #endregion
 
     IEnumerator Sequence() {
-        yield return null;
+        SetSpawner(5);
+        SetSpawner(6);
+        SetSpawner(7);
+        SetSpawner(8);
+        SetSpawner(9);
+        SetSpawner(10);
+        SetSpawner(11);
+        SetSpawner(12);
+
+        yield return new WaitForSeconds(8f);
+
+        SetSpawner(0);
+        SetSpawner(1);
+
+        yield return new WaitForSeconds(35f);
+
+        SetSpawner(2);
+        SetSpawner(3);
+
+        SetSpawner(13);
+        SetSpawner(14);
+        SetSpawner(15);
+
+        yield return new WaitForSeconds(35f);
+
+        SetSpawner(4);
     }
 
     private void SetSpawner(int index) {
